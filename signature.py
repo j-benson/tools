d60eb455b8d65d19e2fde1302d3462d045b2aac7
James Benson <james.benson@sainsburys.co.uk>
2021-05-28 12:07:29 +0100

    md5 signature

---
+++ signature.py
@@ -0,0 +1,16 @@
+#!/usr/local/env python3
+
+import time
+import argparse, hashlib
+
+if __name__ == '__main__':
+  argp = argparse.ArgumentParser()
+  argp.add_argument('-k', '--key', required=True)
+  argp.add_argument('-s', '--secret', required=True)
+  args = argp.parse_args()
+
+  now = int(time.time())
+  data = args.key + args.secret + str(now)
+  md5 = hashlib.md5()
+  md5.update(bytes(data, 'utf-8'))
+  print(md5.hexdigest())
